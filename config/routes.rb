306e412d9d72c060b685fc3f049579e9616e37ec
Shoabe Shariff <shoabe.shariff@gmail.com>
2012-01-20 08:55:31 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Huddlers::Application.routes.draw do
   resources :pictures
 	resources :posts
   resources :huddlers_card
-
+  resources :achievements
   root :to => "huddlers_card#card"
   # The priority is based upon order of creation:
   # first created -> highest priority.
